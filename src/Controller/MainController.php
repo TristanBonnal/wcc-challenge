b7b67faad09757f753ae68b3f06f5f668d86bf55
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-04-13 18:15:51 +0200

    render argonauts ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArgonautRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(ArgonautRepository $repo): Response
     {
-        $argonauts = 'test';
+        $argonauts = $repo->findAll();
         return $this->render('main/index.html.twig', [
             'argonauts' => $argonauts
         ]);
